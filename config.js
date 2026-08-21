98193c5969771fe0743f963cf62b590c3f1e9d90
Dimitrios <moudiotis.meng@gmail.com>
2026-08-21 18:41:33 +0300

    final changes

--- config.js
+++ config.js
@@ -60,7 +60,7 @@ window.SITE_CONFIG = {
         email:     'moudiotis.meng@gmail.com',
         address:   'Αθήνα - Θεσσαλονίκη',
         addressEn: 'Athens - Thessaloniki',
-        facebook:  '',
+        facebook:  'https://www.facebook.com/61550061850076/mentions/',
         linkedin:  'https://www.linkedin.com/in/moudiotis/',
         mapsUrl:   'https://maps.google.com/maps?q=37.965194,23.745722&hl=el&z=17&output=embed',
     },
